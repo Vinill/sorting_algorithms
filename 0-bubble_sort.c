b00bb4155942c128792e8d59067b11bbb076ebab
Vinill <danimuller758@gmail.com>
2022-06-10 12:30:01 +0000

    push 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,2 +1,19 @@
+#include "sort.h"
 
-void bubble_sort(int *array, size_t size);
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j, swap;
+    for (i = 0; i < size; i++)
+    {
+            for (j = 0; j < size + 1; j++)
+            {
+                    if (array[j] > array[j + 1])
+                    {
+                            swap = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = swap;
+                            print_array(array, size);
+                    }
+            }
+    }
+}
